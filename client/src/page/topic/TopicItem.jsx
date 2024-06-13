bfb807acbcbe60f87b090bba006d22f02b72cd1d
Paga <pagaaverin@gmail.com>
2024-06-13 18:38:04 +0300

    asdada

---
+++ client/src/page/topic/TopicItem.jsx
@@ -0,0 +1,12 @@
+import React from'react';
+function TopicItem({ topic,setTopics }) {
+    
+  return (
+      <div>
+        <h1>Категория</h1>
+        
+        </div>
+  );
+}
+
+export default TopicItem;
